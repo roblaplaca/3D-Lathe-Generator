1a597c0af9bfdef746459c9f3aea6ae8d6111e1e
HUGE | Rob La Placa <rlaplaca@hugeinc.com>
2013-11-09 11:14:31 -0500

    adding styles for project-info allowing numSides to be passed into LatheTool as a parameter

--- assets/js/modules/LatheTool.js
+++ assets/js/modules/LatheTool.js
@@ -1,107 +1,95 @@
 (function() {
-	window.LatheTool = function() {
-		var container;
-
-		var camera, scene, renderer;
-
-		var group, text, plane;
-
-		var targetRotation = 0;
-		var targetRotationOnMouseDown = 0;
-
-		var targetYRotation = 0;
-		var targetYRotationOnMouseDown = 0;
-
-		var mouseX = 0;
-		var mouseXOnMouseDown = 0;
-
-		var mouseY = 0;
-		var mouseYOnMouseDown = 0;
-
-		var windowHalfX = window.innerWidth / 2;
-		var windowHalfY = window.innerHeight / 2;
+	window.LatheTool = function(params) {
+		var opts = $.extend({
+				numSides: 4
+			}, params),
+			$module = opts.instance || null,
+			container,
+			camera, scene, renderer,
+			group, mesh,
+			targetRotation = 0,
+			targetRotationOnMouseDown = 0,
+			targetYRotation = 0,
+			targetYRotationOnMouseDown = 0,
+			mouseX = 0,
+			mouseXOnMouseDown = 0,
+			mouseY = 0,
+			mouseYOnMouseDown = 0,
+			windowHalfX = window.innerWidth / 2,
+			windowHalfY = window.innerHeight / 2;
 
 		function init() {
+			if( $module !== null ) {
+				container = $module.get(0);
 
-			container = document.createElement( 'div' );
-			document.body.appendChild( container );
+				camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 1, 1000 );
+				camera.position.set( 0, 0, 500 );
+
+				scene = new THREE.Scene();
+
+				var light = new THREE.DirectionalLight( 0xffffff );
+				light.position.set( 0, 0, 1 );
+				scene.add( light );
+
+				group = new THREE.Object3D();
+				resetModelPosition();
+				group.rotation.x = -1.57;
+				scene.add( group );
+
+				//////////////
+				var squareShape = new THREE.Shape();
+
+				squareShape.moveTo( 0, 100 );
+				// squareShape.quadraticCurveTo( -300, 20, -100, 0);
+				// .arc ( aX, aY, aRadius, aStartAngle, aEndAngle, aClockwise ) 
+				squareShape.arc(0, 0, 25, 1.57079633, 4.71238898, false);
+				squareShape.moveTo( 0, 85 );
+				squareShape.lineTo( -35, 70 );
+				squareShape.lineTo( -15, 60 );
+				squareShape.lineTo( -15, 0 );
+				squareShape.quadraticCurveTo( -30, -10, -10, -10);
+				squareShape.quadraticCurveTo( -40, 0, -40, -20);
+				squareShape.quadraticCurveTo( -50, -20, -45, -30);
+
+				squareShape.lineTo( 0, -30 );
+				squareShape.moveTo( 0, -30 );
+
+				var heartPoints = squareShape.extractPoints().shape,
+					pts = [];
+
+					console.log("heartPoints: ", heartPoints);
+
+				for(var i=0; i<heartPoints.length; i++) {
+					pts.push(new THREE.Vector3(heartPoints[i].x, 0, heartPoints[i].y));
+				}
+
+				var lathe = new THREE.LatheGeometry( pts, opts.numSides),	
+					latheMaterial = new THREE.MeshBasicMaterial( { color: 0x00aedb, wireframe: true, transparent: true });
+
+				mesh = new THREE.Mesh( lathe, latheMaterial);
+
+				// mesh.position.y = 0;
+				mesh.overdraw = true;
+				mesh.doubleSided = true;
 
 
-			var info = document.createElement( 'div' );
-			info.style.position = 'absolute';
-			info.style.top = '10px';
-			info.style.width = '100%';
-			info.style.textAlign = 'center';
-			info.innerHTML = 'Pawn generated using THREE.js LatheGeometry. <a href="javascript:window.latheTool.save();">Click to save STL</a><br/>Drag to spin';
-			container.appendChild( info );
 
-			camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 1, 1000 );
-			camera.position.set( 0, 0, 500 );
+				group.add( mesh );
+				//////////////
 
-			scene = new THREE.Scene();
+				renderer = new THREE.WebGLRenderer( { antialias: true } );
+				renderer.setSize( window.innerWidth, window.innerHeight );
 
-			var light = new THREE.DirectionalLight( 0xffffff );
-			light.position.set( 0, 0, 1 );
-			scene.add( light );
+				container.appendChild( renderer.domElement );
 
-			group = new THREE.Object3D();
-			resetModelPosition();
-			group.rotation.x = -1.57;
-			scene.add( group );
+				document.addEventListener( 'mousedown', onDocumentMouseDown, false );
+				document.addEventListener( 'touchstart', onDocumentTouchStart, false );
+				document.addEventListener( 'touchmove', onDocumentTouchMove, false );
 
-			//////////////
-			var squareShape = new THREE.Shape();
-
-			squareShape.moveTo( 0, 100 );
-			// squareShape.quadraticCurveTo( -300, 20, -100, 0);
-			// .arc ( aX, aY, aRadius, aStartAngle, aEndAngle, aClockwise ) 
-			squareShape.arc(0, 0, 25, 1.57079633, 4.71238898, false);
-			squareShape.moveTo( 0, 85 );
-			squareShape.lineTo( -35, 70 );
-			squareShape.lineTo( -15, 60 );
-			squareShape.lineTo( -15, 0 );
-			squareShape.quadraticCurveTo( -30, -10, -10, -10);
-			squareShape.quadraticCurveTo( -40, 0, -40, -20);
-			squareShape.quadraticCurveTo( -50, -20, -45, -30);
-
-			squareShape.lineTo( 0, -30 );
-			squareShape.moveTo( 0, -30 );
-
-			var heartPoints = squareShape.extractPoints().shape,
-				pts = [];
-
-				console.log("heartPoints: ", heartPoints);
-
-			for(var i=0; i<heartPoints.length; i++) {
-				pts.push(new THREE.Vector3(heartPoints[i].x, 0, heartPoints[i].y));
+				window.addEventListener( 'resize', onWindowResize, false );
+			} else {
+				throw new Error("instance is a required parameter");
 			}
-
-			var lathe = new THREE.LatheGeometry( pts, 4),	
-				latheMaterial = new THREE.MeshBasicMaterial( { color: 0x00aedb, wireframe: true, transparent: true });
-
-			window.mesh = new THREE.Mesh( lathe, latheMaterial);
-
-			//var mesh = THREE.SceneUtils.createMultiMaterialObject( new THREE.LatheGeometry( pts, 10), [ new THREE.MeshLambertMaterial( { color: 0x0055ff } ), new THREE.MeshBasicMaterial( { color: 0x00cc00, wireframe: true, transparent: true } ) ] );
-
-			// mesh.position.y = 0;
-			mesh.overdraw = true;
-			mesh.doubleSided = true;
-
-
-
-			group.add( mesh );
-			//////////////
-
-			renderer = new THREE.WebGLRenderer( { antialias: true } );
-			renderer.setSize( window.innerWidth, window.innerHeight );
-
-			container.appendChild( renderer.domElement );
-
-			document.addEventListener( 'mousedown', onDocumentMouseDown, false );
-			document.addEventListener( 'touchstart', onDocumentTouchStart, false );
-			document.addEventListener( 'touchmove', onDocumentTouchMove, false );
-
-			window.addEventListener( 'resize', onWindowResize, false );
 		}
 
 		function onWindowResize() {
